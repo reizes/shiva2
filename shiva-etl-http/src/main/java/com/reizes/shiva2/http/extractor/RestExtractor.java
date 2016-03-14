fc398f13d67d76bb51bed2279c088ed5c544d327
Kane Nam <reizes0@gmail.com>
2016-03-14 11:34:17 +0900

    implement default rest extractor

--- shiva-etl-http/src/main/java/com/reizes/shiva2/http/extractor/RestExtractor.java
+++ shiva-etl-http/src/main/java/com/reizes/shiva2/http/extractor/RestExtractor.java
@@ -1,13 +1,36 @@
 package com.reizes.shiva2.http.extractor;
 
+import java.io.InputStream;
+import java.net.URISyntaxException;
+import java.util.Map;
+
 import com.reizes.shiva2.etl.core.extractor.AbstractExtractor;
+import com.reizes.shiva2.http.RestClient;
 
 public class RestExtractor extends AbstractExtractor {
-
+	private RestClient client;
+	private Map<String, String> headers;
+	
+	public RestExtractor(String host) throws URISyntaxException {
+		this.client = new RestClient(host);
+	}
+	
 	@Override
-	public Object doProcess(Object input) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+	public Object doProcess(Object requestUri) throws Exception {
+		InputStream is = this.client.get((String)requestUri, headers);
+		
+		startProcessItem(is);
+		
+		is.close();
+		return requestUri;
+	}
+
+	public Map<String, String> getHeaders() {
+		return headers;
+	}
+
+	public void setHeaders(Map<String, String> headers) {
+		this.headers = headers;
 	}
 
 }
